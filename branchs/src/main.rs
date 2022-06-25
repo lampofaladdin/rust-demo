97ec0ea5245614219065a780aa56629097abcc51
aladdin <wangdingding@42how.com>
2022-06-25 22:30:33 +0800

    feat: 控制流

--- branchs/src/main.rs
+++ branchs/src/main.rs
@@ -1,3 +1,55 @@
 fn main() {
-    println!("Hello, world!");
+    // let x = 3;
+    // if x > 3 {
+    //     println!("小于5");
+    // } else {
+    //     println!("大于5");
+    // }
+
+    // let condition = false;
+    // let number = if condition { 5 } else { 6 };
+    // println!("The value of number is: {}", number);
+    // let mut count = 0;
+    // 'count_loop: loop {
+    //     println!("count = {}", count);
+    //     let mut remain = 10;
+    //     loop {
+    //         println!("remain = {}", remain);
+    //         if remain == 9 {
+    //             break;
+    //         }
+    //         if count == 2 {
+    //             break 'count_loop;
+    //         }
+    //         remain -= 1
+    //     }
+    //     count += 1;
+    // }
+    // println!("end count = {}", count)
+    // loop 循环
+    // let mut counter = 0;
+    // let result = loop {
+    //     counter += 1;
+    //     if counter == 10 {
+    //         break counter * 2;
+    //     }
+    // };
+    // println!("the counter is {}", counter);
+    // println!("the counter is {}", result);
+
+    // while 循环
+    // let mut counter = 3;
+    // while counter != 0 {
+    //     counter -= 1;
+    //     println!("the counter is {}", counter)
+    // }
+
+    // let a = [1, 2, 3, 4, 5];
+    // for element in a {
+    //     println!("value is {}", element)
+    // }
+
+    // for item in (1..4).rev() {
+    //     println!("item {}", item)
+    // }
 }
