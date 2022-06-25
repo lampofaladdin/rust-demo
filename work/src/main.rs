f6d4c6ad5276aabb02892c5021ef18a19f228b05
aladdin <wangdingding@42how.com>
2022-06-25 23:32:19 +0800

    feat: rust版本斐波那契额数列

--- work/src/main.rs
+++ work/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    println!("请输入n阶 数列");
+    let mut index = String::new();
+    std::io::stdin()
+        .read_line(&mut index)
+        .expect("读取内容失败");
+    let index: i32 = index.trim().parse().expect("不是数字");
+    if index < 2 {
+        println!("结果:{}", index)
+    } else {
+        let mut looper = 0;
+        let mut pre_val: i128 = 0;
+        let mut c_val: i128 = 1;
+        let mut result: i128 = 0;
+        while looper < index {
+            result = pre_val + c_val;
+            pre_val = c_val;
+            c_val = result;
+            looper += 1;
+            println!("结果：{}", result);
+        }
+    }
 }
