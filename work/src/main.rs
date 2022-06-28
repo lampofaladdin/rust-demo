3cbdde5e31da7e58cf24fc45281d423b05152b3f
aladdin <1295106869@qq.com>
2022-06-29 00:33:13 +0800

    feat: 完成所有权测试

--- work/src/main.rs
+++ work/src/main.rs
@@ -1,3 +1,5 @@
+use std::time::SystemTime;
+
 fn main() {
     println!("请输入n阶 数列");
     let mut index = String::new();
@@ -5,19 +7,29 @@ fn main() {
         .read_line(&mut index)
         .expect("读取内容失败");
     let index: i32 = index.trim().parse().expect("不是数字");
-    if index < 2 {
-        println!("结果:{}", index)
+    let sys_time1 = SystemTime::now();
+    feb(index);
+    let sys_time2 = SystemTime::now();
+    let difference = sys_time2
+        .duration_since(sys_time1)
+        .expect("Clock may have gone backwards"); //expect()方法在Ok时返回T值，在Err时panic
+    println!("{:?}", difference)
+}
+
+fn feb(n: i32) -> i128 {
+    if n < 2 {
+        return n.into();
     } else {
         let mut looper = 0;
         let mut pre_val: i128 = 0;
         let mut c_val: i128 = 1;
         let mut result: i128 = 0;
-        while looper < index {
+        while looper < n {
             result = pre_val + c_val;
             pre_val = c_val;
             c_val = result;
             looper += 1;
-            println!("结果：{}", result);
         }
+        return result;
     }
 }
