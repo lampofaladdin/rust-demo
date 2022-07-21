e14d4eab93bcfd2df42b599f40adc2e91fe8f91d
aladdin <1295106869@qq.com>
2022-07-21 14:34:21 +0800

    todo: 做一些练习题

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -61,13 +61,61 @@
 //             DataRow::Float(f) => println!("Float: {}", f),
 //             DataRow::Text(t) => println!("Text: {}", t),
 //         }
-//     }
+//     }  b
 // }
 
-fn main() {
-    let s1 = String::from("tic");
-    let s2 = String::from("tac");
-    let s3 = String::from("toe");
+// fn main() {
+//     let s1 = String::from("tic");
+//     let s2 = String::from("tac");
+//     let s3 = String::from("toe");
 
-    let s = format!("{}-{}-{}", s1, s2, s3);
+//     let s = format!("{}-{}-{}", s1, s2, s3);
+// }
+
+// use std::collections::HashMap;
+
+// fn main() {
+//     let mut socres = HashMap::new();
+//     socres.insert(String::from("hello"), 10);
+//     socres.insert(String::from("word"), 20);
+//     println!("{:?}", socres)
+// }
+
+// use std::collections::HashMap;
+// fn main() {
+//     let teams = vec![String::from("hello"), String::from("word")];
+//     let initial_scores = vec![10, 50];
+//     let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+//     println!("{:?}", scores)
+// }
+
+// fn main() {
+//     let mut scores = HashMap::new();
+//     scores.insert(String::from("Blue"), 10);
+//     // scores.entry(String::from("Yellow")).or_insert(50);
+//     // scores.entry(String::from("Blue")).or_insert(50);
+
+//     println!("{:?}", scores);
+// }
+
+// fn main() {
+//     let text = "hello world wonderful world";
+//     let mut map = HashMap::new();
+//     for word in text.split_whitespace() {
+//         let count = map.entry(word).or_insert(0);
+//         *count += 1;
+//     }
+//     println!("{:?}", map)
+// }
+
+fn get_average(num_array: Vec<i32>) -> i32 {
+    let sum: i32 = num_array.iter().sum();
+    let average = sum.;
+    println!("{}", average);
+    average
+}
+
+fn main() {
+    let num_array = vec![1, 2, 3, 4, 5];
+    get_average(num_array);
 }
