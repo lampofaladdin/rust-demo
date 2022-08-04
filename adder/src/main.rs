9d105b6114b70a554fc2733561866e257807703d
aladdin <1295106869@qq.com>
2022-08-04 12:05:29 +0800

    feat: 完成测试

--- adder/src/main.rs
+++ adder/src/main.rs
@@ -1,13 +1,15 @@
 
-fn main() {}
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
+// fn main() {}
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
 
-impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
-}
+// impl Rectangle {
+//     fn can_hold(&self, other: &Rectangle) -> bool {
+//         self.width > other.width && self.height > other.height
+//     }
+// }
+
+fn main(){}
