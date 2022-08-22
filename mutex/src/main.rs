ba79c5bedce1344f478dea6cda7b777ad170fa8f
aladdin <1295106869@qq.com>
2022-08-22 13:39:00 +0800

    todo: mutex

---
+++ mutex/src/main.rs
@@ -0,0 +1,20 @@
+use std::rc::Rc;
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+fn main() {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handlers = vec![];
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handler = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handlers.push(handler);
+    }
+    for handler in handlers {
+        handler.join().unwrap();
+    }
+    println!("Result: {}", counter.lock().unwrap());
+}
