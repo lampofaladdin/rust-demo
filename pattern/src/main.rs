cd5ec42ce04659541c0a9aa68411d3b5462c6ef3
aladdin <1295106869@qq.com>
2022-08-31 13:05:04 +0800

    feat: unsafe

--- pattern/src/main.rs
+++ pattern/src/main.rs
@@ -35,12 +35,83 @@
 //     }
 // }
 
-fn print_coordinates(&(x, y): &(i32, i32)) {
-    println!("the value x {},the value y {}", x, y);
+// fn print_coordinates(&(x, y): &(i32, i32)) {
+//     println!("the value x {},the value y {}", x, y);
+// }
+
+// fn main() {
+//     let x = 1;
+//     let y = 2;
+//     print_coordinates(&(x, y));
+//     let b = Some(1);
+//     let Some(x) = b;
+// }
+
+// fn main() {
+//     let x = Some(5);
+//     let y = 10;
+
+//     match x {
+//         Some(50) => println!("Got 50"),
+//         Some(y) => println!("Matched, y = {:?}", y),
+//         _ => println!("Default case, x = {:?}", x),
+//     }
+
+//     println!("at the end: x = {:?}, y = {:?}", x, y);
+// }
+
+// fn main() {
+//     let x = 99;
+
+//     match x {
+//         1 | 2 => println!("one or two"),
+//         3 => println!("three"),
+//         _ => println!("anything"),
+//     }
+// }
+
+// fn main() {
+//     let x = 5;
+
+//     match x {
+//         1..=5 => println!("one through five"),
+//         _ => println!("something else"),
+//     }
+// }
+
+struct Point {
+    x: i32,
+    y: i32,
+    z: i32,
 }
 
 fn main() {
-    let x = 1;
-    let y = 2;
-    print_coordinates(&(x, y));
+    let p = Point { x: 0, y: 7, z: 8 };
+
+    let Point { x: a, y: b, .. } = p;
+    assert_eq!(0, a);
+    assert_eq!(7, b);
 }
+
+// struct Point {
+//     x: i32,
+//     y: i32,
+// }
+
+// fn main() {
+//     let p = Point { x: 0, y: 7 };
+
+//     let Point { x, y } = p;
+//     assert_eq!(0, x);
+//     assert_eq!(7, y);
+// }
+
+// fn main() {
+//     let p = Point { x: 0, y: 7 };
+
+//     match p {
+//         Point { x, y: 0 } => println!("On the x axis at {}", x),
+//         Point { x: 0, y } => println!("On the y axis at {}", y),
+//         Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+//     }
+// }
