9c2d608eeefc481beb5a3949384f3b7ca3fd71bf
aladdin <1295106869@qq.com>
2022-11-07 14:01:33 +0800

    feat: 输出内容

--- print-display/src/main.rs
+++ print-display/src/main.rs
@@ -1,15 +1,38 @@
-use std::fmt::{self};
+use std::fmt::{self, write};
 
 #[derive(Debug)]
-struct Structure(i32);
+struct MinMax(i64, i64);
 
-impl fmt::Display for Structure {
+ 
+
+#[derive(Debug)]
+struct Point2D {
+    x: f64,
+    y: f64,
+}
+
+impl fmt::Display for Point2D {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{} fine yingyingying ", self.0)
+        write!(f, "x : {} ,y : {}", self.x, self.y)
     }
 }
 
 fn main() {
-    let d = Structure(2);
-    println!("{:?}", d);
+    let minmax = MinMax(0, 14);
+    println!("Compare Struct");
+    println!("Dsiplay: {}", minmax);
+    println!("Debug: {:?}", minmax);
+
+    let big_range = MinMax(-300, 300);
+    let small_range = MinMax(-3, 3);
+    println!(
+        "the big range is {big},the small range is {small}",
+        big = big_range,
+        small = small_range
+    );
+
+    let point = Point2D { x: 3.3, y: 7.2 };
+    println!("Compare points");
+    println!("Dsiplay: {}", point);
+    println!("Debug: {:?}", point);
 }
