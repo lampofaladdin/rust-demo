3646f07344e95d186949679a5a3b0a123d2ad5b4
aladdin <1295106869@qq.com>
2022-11-22 13:25:44 +0800

    feat:custom enum use

---
+++ custom-enum-use/src/main.rs
@@ -0,0 +1,28 @@
+#![allow(dead_code)]
+
+enum Status {
+    Rich,
+    Poor,
+}
+
+enum Work {
+    Civilian,
+    Solider,
+}
+
+fn main() {
+    use Status::{Poor, Rich};
+
+    use Work::*;
+
+    let status = Poor;
+
+    let work = Civilian;
+
+    match status {
+        Rich => println!("The rich have lots of money..."),
+        Poor => println!("The poor have no money"),
+    }
+
+    match work {}
+}
