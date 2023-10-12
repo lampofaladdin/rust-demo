70d51769043952d3703493b65b02017d8938c14a
aladdinm <1295106869@qq.com>
2023-10-13 01:49:15 +0800

    feat: 变量

--- second/branches/src/main.rs
+++ second/branches/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    // let number = 3;
+    // if number < 3 {
+    //     println!("The value is less than 3")
+    // } else {
+    //     println!("The value is more than or equal 3");
+    // }
+
+    // let condition = true;
+    // let number = if condition {
+    //     let x = 1;
+    //     x
+    // } else {
+    //     let y = 2;
+    //     y
+    // };
+    // println!("The value of number {number}");
+
+    let mut counter = 0;
+    let result = loop {
+        counter = counter + 1;
+        if counter > 100 {
+            break counter;
+        }
+    };
+    println!("The value of result {result}");
 }
