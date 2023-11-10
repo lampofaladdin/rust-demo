326d81a83b22c741a29eb639bf994fe7e01fdcb9
aladdin <wangdingding@42how.com>
2023-11-10 12:20:52 +0800

    feat: 模块层级问题

---
+++ second/restaurant/src/lib.rs
@@ -0,0 +1,25 @@
+// pub mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
+//     }
+// }
+
+// pub fn eat_at_restaurant() {
+//     crate::front_of_house::hosting::add_to_waitlist();
+//     front_of_house::hosting::add_to_waitlist();
+//     // // 绝对路径
+//     // crate::front_of_house::hosting::add_to_waitlist();
+
+//     // // 相对路径
+//     // front_of_house::hosting::add_to_waitlist();
+// }
+// mod front_of_house
+
+// mod front_of_houses;
+// pub use crate::front_of_houses::hosting;
+
+// pub fn demo() {
+//     hosting::add_to_waitlist();
+// }
+
+mod front_of_house;
